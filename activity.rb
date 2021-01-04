f729c73564acc143cf95bb7e57d0d025a61d9609
Francesco Canessa - @makevoid <makevoid@gmail.com>
2021-01-04 17:58:13 +0000

    cleanup activity .rb with unnecessary complexity

--- activity.rb
+++ activity.rb
@@ -11,10 +11,6 @@ STATS = {
 
 }
 
-STATS_BIG = {
-
-}
-
 def extract_user_id(user:, author:)
   email = user && user.email
   email = author.email if !email || email == ""
@@ -36,11 +32,9 @@ repo.refs.edges.map do |ref_edge|
     user   = author.user
     user_id  = extract_user_id user: user, author: author
     additions = commit.additions
-    additions = :BIG if additions > 300
     stat = STATS[user_id.to_sym] || 0
-    STATS[user_id.to_sym] = stat + additions unless additions == :BIG
-    stat = STATS_BIG[user_id.to_sym] || 0
-    STATS_BIG[user_id.to_sym] = stat + 1 if additions == :BIG
+    STATS[user_id.to_sym] = stat + additions
+    # NOTE: extract statistic relative to your metric / zube card cross referencing like timestamps to compare it to actions/velocity and to change of status (the work is efficiently picked up / devs don't get stuck gathering requirements and open straight away a WIP PR [current best practice])
     puts Time.parse(commit.committed_date).strftime("%b %d")
     puts "#{user_id}: #{additions}"
     puts
@@ -52,15 +46,5 @@ puts "Date: #{Time.now.strftime("%b %d")}"
 puts "Weeks: #{GH::NUM_WEEKS}"
 puts "\n\n"
 
-puts "STATS:"
-puts "-"*8
-STATS.sort_by{ |k, v| -v }.each do |user, value|
-  puts "#{user}: #{value}"
-end
-puts "\n\n"
-puts "STATS_BIG:"
-puts "-"*8
-STATS_BIG.sort_by{ |k, v| -v }.each do |user, value|
-  puts "#{user}: #{value}"
-end
-puts "\n\n"
+# TODO: output prettyfication code from
+p STATS
